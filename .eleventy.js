7abd31eab345d3ab1959781cf625caa5b4cc1546
Matthew Passmore <matthew@matthewpassmore.co.uk>
2020-03-31 09:48:12 +0100

    chore (eleventy): add markdown filter

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,16 @@
 const CleanCSS = require("clean-css");
+const markdownIt = require('markdown-it')({
+  html: true,
+  breaks: true,
+  linkify: true
+});
+
 
 module.exports = function(eleventyConfig) {
+
+  eleventyConfig.addFilter('markdownFilter', function(value) {
+    return markdownIt.render(value);
+  });
 
   // A useful way to reference the context we are running eleventy in
   let env = process.env.ELEVENTY_ENV;
